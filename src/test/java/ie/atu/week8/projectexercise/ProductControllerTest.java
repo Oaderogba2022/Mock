b1e35ddd450e791f1281a814e1b0b5c33a2593d2
Olamide Aderogba <G00401971@atu.ie>
2024-12-02 12:59:24 +0000

    Mock test for ProductControllerTest, for test Get and test Create.

--- src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
@@ -1,12 +1,29 @@
 package ie.atu.week8.projectexercise;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@WebMvcTest(ProductController.class)
 
 class ProductControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
     @BeforeEach
     void setUp() {
     }
@@ -16,15 +33,32 @@ class ProductControllerTest {
     }
 
     @Test
-    void testGetProductById() {
+    void testGetProductById() throws Exception {
+        Product product = new Product(1L, "Kettle", "Boils", 35 );
+        when(productService.getProductById(1L)).thenReturn(Optional.of(product));
+
+        mockMvc.perform(get("/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Kettle"));
+
     }
 
     @Test
-    void createProduct() {
+    void testCreateProduct() throws Exception {
+        Product product = new Product(null, "Kettle", "Boils", 35 );
+        when(productService.saveProduct(any(Product.class))).thenReturn(product);
+
+        ObjectMapper om = new ObjectMapper();
+        String valueJson = om.writeValueAsString(product);
+
+        om.writeValueAsString(product);
+        mockMvc.perform(post("/products").contentType("application/json").content(valueJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Kettle"));
     }
 
     @Test
-    void updateProduct() {
+    void testUpdateProduct() {
     }
 
     @Test
